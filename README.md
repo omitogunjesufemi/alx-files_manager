8cd457cd618d8d22242f1f84c729180320ea6d4b
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-08-06 08:13:29 +0100

    Setting up the project structure redis client for the project

---
+++ README.md
@@ -0,0 +1,12 @@
+# 0x04. Files Manager
+
+## Learning Objectives
+This project helped to understand:
+- how to create an API with Express
+- how to authenticate a user
+- how to store data in MongoDB
+- how to store temporary data in Redis
+- how to setup and use a background worker
+
+## Installing dependencies
+`npm install`
---
+++ utils/redis.js
@@ -0,0 +1,40 @@
+import { createClient } from 'redis'
+import { promisify } from 'util'
+
+class RedisClient {
+    constructor () {
+        this.client = createClient();
+
+        this.client.on('error', (error) => {
+            console.error(`Redis client not connected to the server: ${error}`);
+        });
+
+        this.connected = true;
+
+        this.getAsync = promisify(this.client.get).bind(this.client);
+
+        this.setAsync = promisify(this.client.set).bind(this.client);
+
+        this.delAsync = promisify(this.client.del).bind(this.client);
+    }
+
+    isAlive () {
+        return this.connected;        
+    }
+
+    async get (key) {
+        return await this.getAsync(key);
+    }
+
+    async set (key, value, timeout) {
+        await this.setAsync(key, value, 'EX', timeout);
+    }
+
+    async del (key) {
+        await this.delAsync(key);
+    }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
